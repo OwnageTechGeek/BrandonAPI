97ca9cc4f04a905f23b3a9fca82266b69b465195
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2016-10-17 16:27:48 -0400

    Update Mortgage.java

--- src/Math/Mortgage.java
+++ src/Math/Mortgage.java
@@ -18,10 +18,48 @@ public class Mortgage {
         double monthlyRate = decannualRate / 12;
         double R = monthlyRate;
         double monthlyPayment = (((exponent((1 + R),N) * R) / ((exponent((1 + R),N)) - 1)) * P);
+        if (round >= 0) {
+        double finalmonthlyPayment = rounder(monthlyPayment, round);
+        }
         double finalmonthlyPayment = rounder(monthlyPayment, round);
         double totalPayments = finalmonthlyPayment * months;
         double totalInterest = totalPayments - principal; 
         return monthlyPayment;
+        }
+            
+        public static double interestPayed(double principal, double annualRate, int years, int round) {
+        double months = years * 12;
+        double N = months;
+        double P = principal;
+        double decannualRate = annualRate / 100;
+        double monthlyRate = decannualRate / 12;
+        double R = monthlyRate;
+        double monthlyPayment = (((exponent((1 + R),N) * R) / ((exponent((1 + R),N)) - 1)) * P);
+        if (round >= 0) {
+        double finalmonthlyPayment = rounder(monthlyPayment, round);
+        }
+        double totalPayments = finalmonthlyPayment * months;
+        double totalInterest = totalPayments - principal; 
+        return totalInterest;
+        }
+  
+        public static double totalPayments(double principal, double annualRate, int years, int round) {
+        double months = years * 12;
+        double N = months;
+        double P = principal;
+        double decannualRate = annualRate / 100;
+        double monthlyRate = decannualRate / 12;
+        double R = monthlyRate;
+        double monthlyPayment = (((exponent((1 + R),N) * R) / ((exponent((1 + R),N)) - 1)) * P);
+        if (round >= 0) {
+        double finalmonthlyPayment = rounder(monthlyPayment, round);
+        }
+        double finalmonthlyPayment = rounder(monthlyPayment, round);
+        double totalPayments = finalmonthlyPayment * months;
+        double totalInterest = totalPayments - principal; 
+        return totalPayments;
+        }
+    
     }
     
 }
