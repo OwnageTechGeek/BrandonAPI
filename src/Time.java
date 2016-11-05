20ffeb288a7a6dec7e93e6fe554c9b300db6eff4
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2016-11-04 23:59:26 -0400

    Create Time.java

---
+++ src/Time.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author hogueb10289
+ */
+public class Mortgage {
+    
+        public static void sleep(int x) {
+        Thread.sleep(x);
+        }
+        
+        
